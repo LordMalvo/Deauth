562475a2e9b82c762aeff7d8d680a290adb1b1c6
Pablo Vilallave <lordmalvo@localhost.localdomain>
2021-05-12 18:19:44 +0200

    Titulo añadido

---
+++ deauth.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Estilos
+BOLD_RED="\e[1;31m"
+BOLD_WHITE="\e[1;37m"
+BOLD_PURPLE="\e[1;35m"
+
+
+function titulo(){
+    echo -e $BOLD_RED
+    echo  " ______                        __                            __    __ "
+    echo  "/\  _  \  __                  /\ \                          /\ \__/\ \ "
+    echo  "\ \ \L\ \/\_\  ____           \_\ \    ____   ____    __  __\ \ ,_\ \ \____"
+    echo  " \ \  __ \/\ \/\ __\ _______  /_   \  / __ \ / __ \  /\ \/\ \\\ \ \/\ \   _ \ "
+    echo  "  \ \ \/\ \ \ \ \ \//\______\/\ \L\ \/\  __//\ \L\.\_\ \ \_\ \\\ \ \_\ \ \ \ \\ "
+    echo  "   \ \_\ \_\ \_\ \_\\\/______/\ \___,_\ \____\ \__/.\_\\\ \____/ \ \__\\\ \_\ \_\ "
+    echo  "    \/_/\/_/\/_/\/_/          \/__,_ /\/____/\/__/\/_/ \/___/   \/__/ \/_/\/_/"
+    echo -e $BOLD_WHITE
+    echo  "     Deautentificador via Aircrack.ng por Pablo Vilallave"
+}
+
+titulo
+
+
